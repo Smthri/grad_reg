1ae13d020b6a3316e746b83a85fc0e7b88aa5ad2
Smthri <lockshin1999@mail.ru>
2021-03-09 22:02:27 +0300

    cleaning

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 import os
 import numpy as np
 from tqdm import tqdm
+from loss import GradRegLoss
 
 class Net(nn.Module):
     def __init__(self, BATCH_SIZE):
@@ -21,7 +22,7 @@ class Net(nn.Module):
         self.device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
         self.to(self.device)
 
-        self.loss_fn = nn.CrossEntropyLoss()
+        self.loss_fn = GradRegLoss()
         self.optimizer = optim.Adam(self.parameters(), lr=0.001, weight_decay=5e-04)
         self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer)
         self.bs = BATCH_SIZE
@@ -42,69 +43,3 @@ class Net(nn.Module):
             self.load_state_dict(torch.load(path))
         else:
             print(f'WARNING: path {path} does not exist!')
-    
-    def fit(self, trainloader, epochs, val_loader=None, callbacks=None):
-        min_acc = -np.inf
-        for epoch in range(epochs):  # loop over the dataset multiple times
-
-            epoch_loss = 0.0
-            epoch_acc = 0.
-            val_acc = -np.inf
-            val_loss = np.inf
-            pbar = tqdm(enumerate(trainloader, 0))
-            for i, data in pbar:
-                # get the inputs; data is a list of [inputs, labels]
-                inputs, labels = data[0].to(self.device), data[1].to(self.device)
-
-                # zero the parameter gradients
-                self.optimizer.zero_grad()
-
-                # forward + backward + optimize
-                outputs = self(inputs)
-                loss = self.loss_fn(outputs, labels)
-                loss.backward()
-                self.optimizer.step()
-
-                pred = torch.argmax(outputs, axis=1)
-                running_acc = (pred == labels).float().sum() / self.bs * 100
-                running_loss = loss.item()
-
-                epoch_acc += running_acc
-                epoch_loss += running_loss
-
-                # print statistics
-                pbar.set_description(f'Done {(i + 1)/len(trainloader) * 100}%, Epoch: {epoch}, Loss: {running_loss}, Accuracy: {running_acc}%')
-                pbar.update(self.bs)
-            pbar.close()
-            
-            
-            if not val_loader is None:
-                val_acc, val_loss = self.test(val_loader)
-                
-            self.scheduler.step(val_loss)
-            
-            print(f'Avg loss: {epoch_loss / len(trainloader)}, Avg accuracy: {epoch_acc / len(trainloader)}, Val loss: {val_loss}, Val accuracy: {val_acc}')
-            
-            if val_acc > min_acc:
-                print(f'Improved val acc from {min_acc} to {val_acc}, saving model')
-                min_acc = val_acc
-                self.save()
-            else:
-                print(f'Val acc did not improve from {min_acc}')
-
-        print('Finished Training')
-
-    def test(self, testloader):
-        correct = 0.
-        total = 0.
-        loss = 0.
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data[0].to(self.device), data[1].to(self.device)
-                outputs = self(images)
-                loss += self.loss_fn(outputs, labels).item()
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-
-        return correct / total * 100, loss / total
