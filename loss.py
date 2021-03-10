36d5c325d0c584d31466e860b675da93f973cc82
Smthri <lockshin1999@mail.ru>
2021-03-10 15:51:21 +0300

    start fgsm

--- loss.py
+++ loss.py
@@ -6,10 +6,12 @@ class GradRegLoss():
         self.base_loss = nn.CrossEntropyLoss()
         self.hp = hyperparam
 
-    def __call__(self, output, target, inputs):
-        cross_entropy = self.base_loss(output, target)
-        grads = torch.autograd.grad(cross_entropy, inputs, create_graph=True)[0]
-
-        reg = self.hp * torch.sum(grads ** 2) + cross_entropy
+    def __call__(self, output, target, inputs, train=True):
+        if train:
+            cross_entropy = self.base_loss(output, target)
+            grads = torch.autograd.grad(cross_entropy, inputs, create_graph=True)[0]
+            reg = self.hp * torch.sum(grads ** 2) + cross_entropy
+        else:
+            reg = self.base_loss(output, target)
 
         return reg
